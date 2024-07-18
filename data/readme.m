e59dd3bf364ca26ef8324224ac44fc73ea7c7260
lwk98 <37585808+lwk98@users.noreply.github.com>
2024-07-18 09:18:09 +0800

    Update readme.m

--- data/readme.m
+++ data/readme.m
@@ -1,3 +1,1 @@
-% glass:原来有10个 特征，去掉了第一个序号ID，现在只有9个
-%musk: Please note that the molecule_name and conformation_name attributesshould not be used to predict the class.去掉两列.  
- %ionosphere:原先的第二列特征全是0，直接去掉了，在得出结果时需要注意。
+
